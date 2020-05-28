2a72a07e60dcb21185d9210dbf4089ad47d195fc
voron434 <070809010@mail.ru>
2020-05-28 16:10:02 +0300

    fix names

--- districts/apps.py
+++ districts/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class HelloWorldConfig(AppConfig):
+class DistrictsConfig(AppConfig):
     name = 'districts'
--- districts/urls.py
+++ districts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from hello_world import views
+from districts import views
 
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
